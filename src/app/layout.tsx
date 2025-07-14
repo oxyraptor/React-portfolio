57f20c59d19372a054fea5e79e6dc48f13dc5a86
mandar <134038514+oxyraptor@users.noreply.github.com>
2025-07-14 20:37:56 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Mandar Portfolio',
+  title: 'Mandar Jadhav',
   description: 'portfolio of Mandar, an AIML enthusiast,developer,  and 3D artist .',
 };
 
